e19ab7c7524f1879896bf9e8a89de4f584263d8e
Allegra Santoso <allegrasantoso@gmail.com>
2026-01-07 23:37:08 +0700

    Set up Docker environment for FastAPI with Postgres and Redis

--- app/main.py
+++ app/main.py
@@ -2,7 +2,11 @@ from fastapi import FastAPI
 
 from app.routers.products import router as products_router
 
-app =FastAPI(title="Distributed E-Commerce Order System - Chronicle Backend Techincal Test")
+from app.db.database import engine, Base
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI(title="Distributed E-Commerce Order System - Chronicle Backend Techincal Test")
 
 @app.get("/health")
 def health_check():
